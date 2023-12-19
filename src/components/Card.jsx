2caa7e747f6832de8b503dba62611218626f554b
HexPrism <bharathkrishnan33@gmail.com>
2023-12-19 12:03:29 +0530

    Working Card Stack with minor Issues

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,23 @@
-const Card = ({size_var,data,domDisplayStyle}) => {
+'use client'
+import { useEffect, useState } from "react"
+
+const Card = ({size_var,data,width,domDisplayStyle}) => {
+    const [click,setClick] = useState(false);
+    const [hover,setHover] = useState(false);
+
+    useEffect(()=>{
+        if(!hover)
+            setClick(false);
+    },[hover])
+
     return (
-        <div className={`${domDisplayStyle} text-center bg-green-200 w-32 h-32 
-                 shadow-xl hover:w-48 hover:h-48 hover:shadow-2xl transition-all duration-300 m-auto`}
-             style={{left:`${size_var}px` }}
+        <div
+            onClick={()=>setClick(true)}
+            onMouseEnter={() => setHover(true)}
+            onMouseLeave={() => setHover(false)}
+            className={`${domDisplayStyle} text-center bg-green-200 w-32 h-32 ${click ? "z-10" : "z-0"} 
+                 shadow-xl hover:w-44 hover:h-44 hover:shadow-2xl transition-all duration-300 m-auto`}
+             style={click ? {left : `${size_var + width/2}px`}:{left:`${size_var}px`}}
         >
             Card {data}
         </div> 
--- src/components/CardHolder.jsx
+++ src/components/CardHolder.jsx
@@ -4,7 +4,7 @@ const CardHolder = () => {
 
     //From 4 cards onwards things will start stacking 
 
-    const arr = [1,2,3,4]//Kinda card Array
+    const arr = [1,2,3,4,5,6]//Kinda card Array
     const width = -60; 
     let domDisplayStyle = "flex"
     let changeDisplay = (arr.length > 3);
@@ -13,15 +13,18 @@ const CardHolder = () => {
 
     console.log()
     return (
-        <div className={`${changeDisplay ? "flex" : "block"} justify-around`}>
-            <div className="flex justify-around" 
+        <div className={`${changeDisplay ? "flex" : "block"} justify-around p-5`}>
+            <div className="flex justify-around m-auto" 
                 style={changeDisplay ? {paddingLeft : `${-width * (arr.length - 1)}px`} : {paddingLeft: `0px`}}>
                 {
                     arr.map((data,i) =>{
-                        const size_var = i * width; 
-                        console.log(size_var);
+                        const size_var = (i+1) * width; 
                         return(
-                            <Card size_var={size_var} data = {data} domDisplayStyle={domDisplayStyle} key={i}/>
+                            <Card   size_var={size_var} 
+                                    width={width} 
+                                    data = {data} 
+                                    domDisplayStyle={domDisplayStyle} 
+                                    key={i}/>
                         );
                     }
                     )
