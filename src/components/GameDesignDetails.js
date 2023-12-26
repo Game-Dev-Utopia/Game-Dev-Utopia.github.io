335f6153e9a71f78ba7304dc4bd0aeb562de1eb9
adityamore7000 <adi.more7000@gmail.com>
2023-12-26 08:44:50 +0530

    add games-design-component

---
+++ src/components/GameDesignDetails.js
@@ -0,0 +1,18 @@
+
+export default function GameDesignDetails({ isGame}) {
+  return (
+    <section>
+        <div className="grid grid-cols-2">
+            <div>
+                
+            </div>
+            <div>
+
+            </div>
+        </div>
+        <div>
+
+        </div>
+    </section>
+  )
+}
