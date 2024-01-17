1f05b7753d4cd4877ce57af2c20f19794b9376cb
HexPrism <bharathkrishnan33@gmail.com>
2024-01-17 20:33:46 +0530

    Minor Responsive Fixes

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -1,8 +1,16 @@
+'use client'
 import TimelineCard from './TimelineCard';
+import { useEffect, useRef} from 'react';
 
 const Timeline = ({ datas }) => {
+    const ScrollRef = useRef(null); 
+
+    useEffect(()=>{
+        console.log(ScrollRef.scrollTop);
+    },[])
+
     return (
-        <div className='h-[90vh] overflow-y-scroll overflow-x-hidden'>
+        <div ref={ScrollRef} className='Scroll h-[90vh] overflow-y-scroll overflow-x-hidden'>
             <div className='Timeline'>
                 {datas.map((data, i) => <TimelineCard key={i} data={data} /> )}
             </div>
--- src/components/TimelineCard.jsx
+++ src/components/TimelineCard.jsx
@@ -1,23 +1,24 @@
+'use client'
 import Image from 'next/image';
 
 const TimelineCard = ({ data }) => {
-    console.log(1);
     return (
         <div className='Achievement'>
             <Image 
+                alt={data.name}
                 src={data.image}
                 width="300"
                 height="300"
                 className='w-[3%] h-auto z-20 absolute rounded-[50%] border-white border-[0.3vw] left-[80%] scale-150 md:left-1/2 md:scale-100 bottom-1/2 -translate-x-1/2 translate-y-1/2'
             />
-            <p className='Achievement-time'>{data.date}</p>
+            <p className='Achievement-time text-sm md:text-lg '>{data.date}</p>
             <div className='Achievement-tag'></div>
-            <div className='Achievement-detail'>
+            <div className='Achievement-detail text-[2vw] md:text-[0.9vw]'>
                 <div className='Achievement-detail-inner'>
                 </div>
                 <h1 className='Achievement-title'><b>{data.title}</b></h1>
                 <h3 className='Achievement-title'>{data.altName}</h3>
-                <ul className='Achievement-details list-disc'>
+                <ul className='Achievement-details list-disc'> 
                     { data.details.map((detail, i) => <li key={i}>{detail}</li>) }
                 </ul>
             </div>
