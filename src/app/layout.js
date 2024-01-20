2889dcf9e7cc19567bf50c5fd8e50d51fce3787b
MayuriKolhe-2003 <kolhem10@gmail.com>
2024-01-20 22:41:13 +0530

    added forms

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,6 +13,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <Navbar />
+      
       <body className={inter.className}>{children}</body>
     </html>
   )
---
+++ src/utilities/LetsTalkData.js
@@ -0,0 +1,23 @@
+export const Lets_talkData = [
+    {
+        title: "JOIN US",
+        text: "If you go over your organisations or user limit, a member of the team will reach out about bespoke pricing. In the meantime, our collaborative features won't appear in accounts or users that are over the 100-account or 1,000-user limit.",
+        active: false,
+        img : "https://cdni.iconscout.com/illustration/premium/thumb/woman-login-into-banking-app-5194430-4341049.png" ,
+        borderColor: "#44ff00"
+      },
+      {
+        title: "SPONSER US",
+        text: "If you go over your organisations or user limit, a member of the team will reach out about bespoke pricing. In the meantime, our collaborative features won't appear in accounts or users that are over the 100-account or 1,000-user limit.",
+        active: false,
+        img : "https://cdni.iconscout.com/illustration/premium/thumb/login-page-4468581-3783954.png?f=webp",
+        borderColor: "#ebf707"
+      },
+      {
+        title: "CONTACT US",
+        text: "If you go over your organisations or user limit, a member of the team will reach out about bespoke pricing. In the meantime, our collaborative features won't appear in accounts or users that are over the 100-account or 1,000-user limit.",
+        active: false,
+        img:"https://png.pngtree.com/png-clipart/20230504/original/pngtree-free-vector-login-concept-illustration-png-image_9140539.png",
+        borderColor: "#146dfc"
+      },
+]
