36644658450c6ef2ec971cc7d6557b6ed4f21757
adityamore7000 <adi.more7000@gmail.com>
2024-01-27 15:09:32 +0530

    add tooltips and animations to awards, refactor code

--- src/components/GameDesignDetails.js
+++ src/components/GameDesignDetails.js
@@ -1,41 +1,90 @@
 /* eslint-disable @next/next/no-img-element */
-/* eslint-disable react/no-unescaped-entities */
-"use client"
+"use client";
+import React from 'react';
+import './GameDesignDetails.css';
 export default function GameDesignDetails() {
+  const [selectedAwardIndex, setSelectedAwardIndex] = React.useState(0);
+  const acheivementArray =  [
+    "This is a placeholder sentence created for the purpose of filling space in a document or visual presentation.",
+    "Placeholder text helps to demonstrate what the final content might look like in the finished product or design.",
+    "The quick brown fox jumps over the lazy dog, providing a sentence that uses every letter in the alphabet.",
+    "Lorem ipsum is a type of placeholder text used in the publishing and typesetting industry for previewing layouts."
+  ];
   return (
-    <section className="w-[90vw] bg-clip-padding">
-      <div className="grid grid-cols-2 gap-6">
-        <div className=" p-8 rounded-2xl bg-gradient-to-tr from-[#202020] to-[#151515] hover:shadow-md hover:shadow-[#fdb714] duration-200">
-          <h1 className="text-3xl font-semibold text-[#fdb714] mb-2">About the Game</h1>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea sit suscipit sapiente, labore magnam ad numquam ratione amet iusto explicabo ipsum facere iure, voluptatibus consectetur voluptas animi totam voluptatem minima.</p>
-        </div>
-        <div className="p-8 rounded-2xl bg-gradient-to-tr from-[#202020] to-[#151515] hover:shadow-md hover:shadow-[#fdb714] duration-200">
-          <h1 className="text-3xl font-semibold text-[#fdb714] mb-2">Achievements</h1>
-          <div className="flex flex-row gap-2 my-2">
-            <img width={40} src="/trophy.svg" alt="trophy" />
-            <img width={40} src="/trophy.svg" alt="trophy" />
-            <img width={40} src="/trophy.svg" alt="trophy" />
-            <img width={40} src="/trophy.svg" alt="trophy" />
+    <section className="game-design-details">
+      <div className="grid grid-cols-2 gap-6 mx-2">
+        <div className="section-content p-6 rounded-2xl bg-gradient-to-tr from-[#202020] to-[#161616] hover:scale-[1.01] hover:shadow-md hover:shadow-[#fdb714] duration-200">
+          <h1 className="text-3xl text-[#fdb714] font-semibold py-2">About the Game</h1>
+          <div className='glass_bar'>
+            <div className='glass1'></div>
+            <div className='glass3'></div>
+            <div className='glass2'></div>
           </div>
-          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quia, vero consectetur dolore soluta debitis quis. Voluptatibus minima harum sint dolorum, architecto necessitatibus mollitia iste vero et impedit pariatur quisquam iure a quod alias optio accusamus. Quas tempora nisi officiis ullam!</p>
+          <p className='py-6'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea sit
+            suscipit sapiente, labore magnam ad numquam ratione amet iusto
+            explicabo ipsum facere iure, voluptatibus consectetur voluptas animi
+            totam voluptatem minima.
+          </p>
+        </div>
+        <div className="section-content p-6 h-full rounded-2xl bg-gradient-to-tr from-[#202020] to-[#161616]  hover:scale-[1.01] hover:shadow-md hover:shadow-[#fdb714] duration-200">
+          <h1 className="text-3xl text-[#fdb714] font-semibold py-2">Achievements</h1>
+          <div className='glass_bar'>
+            <div className='glass1'></div>
+            <div className='glass3'></div>
+            <div className='glass2'></div>
+          </div>
+          <div className=" gap-2 my-6">
+            <div className='flex flex-row'>{"1234".split("").map((item,i) => (
+              <span
+              onClick={()=>setSelectedAwardIndex(i)} 
+              className={`relative p-3 rounded-md mx-2 duration-100 ${(selectedAwardIndex===i)?'border-2 border-[#fdb714]':'border-2 border-transparent'}`} key={item}>
+              <img src="/trophy.svg" alt="trophy" />
+              <p className='text-lg absolute p-2 -top-2 -right-2'>
+                🥇
+              </p>
+              </span>
+            ))}</div>
+          </div>
+          <p className='py-6'>
+            {acheivementArray[selectedAwardIndex]}
+          </p>
         </div>
       </div>
-      <div className="my-6 p-8 rounded-2xl bg-gradient-to-tr from-[#202020] to-[#151515] hover:shadow-md hover:shadow-[#fdb714] duration-200">
-        <h1 className="text-3xl font-semibold text-[#fdb714] mb-2">System Requirements</h1>
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quidem iure recusandae veritatis nulla placeat tempore aliquid pariatur corrupti! Reprehenderit soluta minus sequi voluptas mollitia fugit atque quia. Pariatur ipsa laudantium fuga consequatur eius ullam nesciunt.</p>
-      </div>
-      <div className="my-6 p-8 rounded-2xl bg-gradient-to-tr from-[#202020] to-[#151515] hover:shadow-md hover:shadow-[#fdb714] duration-200">
-        <h1 className="text-3xl font-semibold text-[#fdb714] mb-2">Developer's Desk</h1>
-        <div>
-          <h3 className="text-xl mb-2">Created By:</h3>
-          <div className="flex gap-2 my-2">
-            {"123".split('').map((item)=>(
-              <img key={item} width={'40px'} src="https://images.rawpixel.com/image_800/czNmcy1wcml2YXRlL3Jhd3BpeGVsX2ltYWdlcy93ZWJzaXRlX2NvbnRlbnQvbHIvdjkzNy1hZXctMTExXzMuanBn.jpg" alt={item} />
-            ))}
-          </div>
+      <div className="section-content m-2 my-4 p-6 rounded-2xl bg-gradient-to-tr from-[#202020] to-[#161616]  hover:scale-[1.01] hover:shadow-md hover:shadow-[#fdb714] duration-200">
+        <h1 className="text-3xl text-[#fdb714] font-semibold py-2">System Requirements</h1>
+        <div className='glass_bar'>
+          <div className='glass1'></div>
+          <div className='glass3'></div>
+          <div className='glass2'></div>
         </div>
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quidem iure recusandae veritatis nulla placeat tempore aliquid pariatur corrupti! Reprehenderit soluta minus sequi voluptas mollitia fugit atque quia. Pariatur ipsa laudantium fuga consequatur eius ullam nesciunt.</p>
+        <p className='py-6'>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quidem iure
+          recusandae veritatis nulla placeat tempore aliquid pariatur corrupti!
+          Reprehenderit soluta minus sequi voluptas mollitia fugit atque quia.
+          Pariatur ipsa laudantium fuga consequatur eius ullam nesciunt.
+        </p>
+      </div>
+      <div className="section-content mx-2 my-4 p-6 rounded-2xl bg-gradient-to-tr from-[#202020] to-[#161616]  hover:scale-[1.01] hover:shadow-md hover:shadow-[#fdb714] duration-200">
+        <h1 className="text-3xl text-[#fdb714] font-semibold py-2">Developer&apos;s Desk</h1>
+        <div className='glass_bar'>
+          <div className='glass1'></div>
+          <div className='glass3'></div>
+          <div className='glass2'></div>
+        </div>
+
+        <span className='flex gap-2 my-6'>
+          <img src="/trophy.svg" alt="" />
+          <img src="/trophy.svg" alt="" />
+          <img src="/trophy.svg" alt="" />
+        </span>
+        <p className='py-6'>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quidem iure
+          recusandae veritatis nulla placeat tempore aliquid pariatur corrupti!
+          Reprehenderit soluta minus sequi voluptas mollitia fugit atque quia.
+          Pariatur ipsa laudantium fuga consequatur eius ullam nesciunt.
+        </p>
       </div>
     </section>
-  )
+  );
 }
--- src/components/Games/index.js
+++ src/components/Games/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 "use client"
 import React, { useState } from "react";
 import "./index.css"; 
@@ -9,6 +10,7 @@ import { AiFillLike } from "react-icons/ai";
 // import snap3 from "/images/image-4.png";
 // import snap4 from "/images/image-5.png";
 import Iframe from "react-iframe";
+import ToolTip from "../ToolTip";
 
 const Games = () => {
   const genre = ["action","adventure","indie"]
@@ -33,7 +35,7 @@ const Games = () => {
 
   return (
     <>
-      <div className="w-[90vw] p-2 rounded-2xl bg-gradient-to-tr from-[#202020] to-[#151515]">
+      <div className="mx-2 p-2 rounded-2xl bg-gradient-to-tr from-[#202020] to-[#151515]">
         <div className="video-game-wrapper bg-gradient">
         <div class="carousel-container mr-2">
   <div class="carousel-wrapper">
@@ -75,7 +77,7 @@ const Games = () => {
 </div>
 
           <div className="video-card h-full border-l-2 border-slate-300 border-opacity-20">
-            <iframe height='400' width="600" src="https://www.youtube.com/embed/bPq776iVXtM?si=Enh2ovrDk2PBt7lg" 
+            <iframe height='400' width="600" src="https://www.youtube.com/embed/raIqPgW-quI" 
             title="YouTube video player" frameborder="0" 
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
             allowfullscreen></iframe>
@@ -88,29 +90,48 @@ const Games = () => {
               <div className="thumbnail">
                 <img src={'/images/image-23.png'} alt="Game Thumbnail" />
               </div>
-              <p>
+              <p className="py-6">
                  Lorem Ipsum has been the industry&apos;s standard dummy
                 text ever since the 1500s, when an unknown printer took a galley
                 of type and scrambled it. 
               </p>
               <div className="buttons flex gap-2 my-2">
-                <button className="py-2"><FaPlay /></button>
-                <button className="py-2"><FaShare /></button>
-                <button className="py-2">
+                <button className="py-2 group">
+                  <FaPlay />
+                  <span className="tooltip group-hover:scale-100">
+                  <p>Play</p>
+                  </span>
+                  </button>
+                <button className="py-2 group">
+                  <FaShare />
+                  <span className="tooltip group-hover:scale-100">
+                  <p>Share</p>
+                  </span>
+                  </button>
+                <button className="py-2 group flex">
                   <a href="https://chouremalhar.itch.io/dicey-roads" target="_blank">
                       <IoMdDownload />
                     </a>
+                    <span className="tooltip group-hover:scale-100">
+                  <p>Download</p>
+                  </span>
                 </button>
-                <button class="py-1 flex items-center gap-[1px]" onClick={handleLike}>
+                <button class="py-1 flex items-center group gap-[1px]" onClick={handleLike}>
                   <AiFillLike />
                   <span>{likes}</span>
+                  <span className="tooltip group-hover:scale-100">
+                  <p>Like</p>
+                  </span>
                 </button>
 
-                <button className="py-2">
+                <button className="py-2 group flex">
                   <FaComment />
+                  <span className="tooltip group-hover:scale-100">
+                  <p>Comment</p>
+                  </span>
                 </button>
               </div>
-              <div className="flex gap-2">
+              <div className="flex gap-2 relative top-3">
                 {genre.map((item) => (
                   <span key={item} className="border-yellow-700 border-2 py-1 rounded-full bg-slate-900 text-gray-300 text-xs px-2">{item}</span>))}
               </div>
@@ -123,3 +144,4 @@ const Games = () => {
 };
 
 export default Games;
+
