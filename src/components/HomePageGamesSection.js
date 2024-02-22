f94ed9938da075f444b7f6bf004e09abc0085d35
adityamore7000 <adi.more7000@gmail.com>
2024-02-22 21:28:48 +0530

    video added

--- src/components/HomePageGamesSection.js
+++ src/components/HomePageGamesSection.js
@@ -10,29 +10,68 @@ import Image from 'next/image'
 // import games_media from '../data/home_page_thumbnails'
 
 import styles from '../styles/HomePageGame.module.css'
-const HomePageGame = ()=>{
-    return(
-        <section className='flex justify-between w-screen h-[80dvh] bg-white relative'>
-            <span className={`w-full ${styles.home_game_1} relative`}>
-                <Image 
-                className='object-cover'
-                src={'/images/home_page_thumbnails/first.png'}
-                fill={true}
-                alt='image-1'/>
+import { useState } from 'react'
+const HomePageGame = () => {
+    const [video,setVideo]  = useState([false,false,false])
+    const [hover, setHover] = useState(false)
+    const handleLeave = (e) => {
+        setVideo(
+            (prev)=>{
+                let temp = [false,false,false]
+                return temp
+            }
+        )
+        setHover(false)
+    }
+
+    const toggleVideo = (i) => {
+        setVideo(
+            (prev)=>{
+                let temp = [false,false,false]
+                temp[i] = !prev[i]
+                return temp
+            }
+        )
+        console.log(video)
+        setHover(true)
+    }
+    return (
+        <section className={'flex h-[80dvh] relative bg-white hover:bg-transparent'}>
+            <span onMouseEnter={()=>toggleVideo(0)}
+                onMouseLeave={handleLeave}
+                onClick={()=>toggleVideo(0)}
+                className={`w-full ${!hover && styles.home_game_1} group bg-slate-950 relative`}>
+                {!video[0]?(<Image
+                    className={'object-fill group-hover:opacity-30 opactiy-100'}
+                    src={'/images/home_page_thumbnails/first.png'}
+                    fill={true}
+                    alt='image-1' />):(
+                    <video className={'object-fill absolute w-[33vw] h-[80vh] left-0  bg-slate-950 group-hover:opacity-30 opactiy-100 duration-200'} src={'/videos/video_1.mp4'} autoPlay={true} loop={true} muted={true} />
+                    )}
+                    <p className={'absolute text-justify px-2 z-10 bottom-1 duration-200 opacity-0 group-hover:opacity-100'}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis accusamus ullam ratione laborum reiciendis distinctio perferendis culpa at. Pariatur, quasi.</p>
             </span>
-            <span className={`w-full ${styles.home_game_2}`} >
-                <Image 
-                className='object-cover -z-10'
-                src={'/images/home_page_thumbnails/second.jpg'}
-                fill={true}
-                alt='image-1'/>
+            <span onMouseEnter={()=>toggleVideo(1)}
+                onMouseLeave={handleLeave}
+                className={`w-full group ${!hover && styles.home_game_2}`} >
+                {!video[1]?(<Image
+                    className='object-fill absolute -z-10 bg-slate-950 group-hover:opacity-30 opactiy-100 duration-200'
+                    src={'/images/home_page_thumbnails/second.jpg'}
+                    fill={true}
+                    alt='image-2' />):(<video className={'object-fill w-[80vw] h-[80vh] absolute -z-10 bg-slate-950 group-hover:opacity-30 left-[10vw] opactiy-100 duration-200'} src={'/videos/video_1.mp4'} autoPlay={true} loop={true} muted={true} />)}
+                    <p className={'absolute text-justify px-2 z-10 bottom-1 duration-200 opacity-0 group-hover:opacity-100 w-[33%]'}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis accusamus ullam ratione laborum reiciendis distinctio perferendis culpa at. Pariatur, quasi.</p>
             </span>
-            <span className={`w-full ${styles.home_game_3} relative`}>
-                <Image 
-                className='object-cover '
-                src={'/images/home_page_thumbnails/third.jpeg'}
-                fill={true}
-                alt='image-1'/>
+            <span
+                onClick={()=>toggleVideo(2)}
+                onMouseLeave={handleLeave}
+                onMouseEnter={()=>toggleVideo(2)} className={` bg-slate-950 w-full group ${!hover && styles.home_game_3} relative`}>
+                {!video[2]?(<Image
+                    className={'object-fill group-hover:opacity-30 opactiy-100'}
+                    src={'/images/home_page_thumbnails/third.jpeg'}
+                    fill={true}
+                    alt='image-1' />):(
+                    <video className={'object-fill absolute w-[33vw] h-[80vh] left-0 group-hover:opacity-30 opactiy-100 duration-200'} src={'/videos/video_1.mp4'} autoPlay={true} loop={true} muted={true} />
+                    )}
+                    <p className={'absolute text-justify px-2 z-10 bottom-1 duration-200 opacity-0 group-hover:opacity-100'}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis accusamus ullam ratione laborum reiciendis distinctio perferendis culpa at. Pariatur, quasi.</p>
             </span>
         </section>
     )
@@ -40,11 +79,11 @@ const HomePageGame = ()=>{
 export default function HomePageGamesSection() {
 
     return (
-    <section >
-        <h2 className="text-5xl text-headings">Games</h2>
-        <div>
-            <HomePageGame/>
-        </div>
-    </section>
-  )
+        <section >
+            <h2 className="text-5xl text-headings">Games</h2>
+            <div>
+                <HomePageGame />
+            </div>
+        </section>
+    )
 }
