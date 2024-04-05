f7c683f78ca07daa2feb9715375e5f4379f78a49
Kiran C. Kankariya <kirankankariya097+git@gmail.com>
2024-04-05 20:41:08 +0530

    Sidebar Navbar Footer changes

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
 import HomePageGamesSection from "@/components/HomePageGamesSection";
 import LandingPage from "@/components/LandingPage/LandingPage";
+import Sponser_Card from "@/components/Sponser_Card";
 
 export default function Home() {
   return (
     <>
-    <LandingPage/>
-    <HomePageGamesSection />
+      <LandingPage />
+      <HomePageGamesSection />
+      <Sponser_Card />
     </>
   )
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,17 +2,33 @@
 import React, { useState } from 'react';
 import * as FaIcons from 'react-icons/fa';
 import { SidebarData } from '../utilities/SidebarData';
-import { IconContext } from 'react-icons';
-import { ImCross } from "react-icons/im";
 import { RxCross2 } from "react-icons/rx";
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import { useSidebar } from '@/contexts/SidebarContextProvider';
+import GduLogo from "../../public/GduLogo.png"
+import Image from 'next/image';
+import { Bounce, ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function Navbar() {
     const [navDescp, setNavDescp] = useState(false)
     const { showSidebar } = useSidebar();
     // console.log(isFooterVisible);
+
+    const bellButtonHandler = () => (
+        toast('🎉🎉Coming Soon!!!', {
+            position: "top-right",
+            autoClose: 2000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "dark",
+            transition: Bounce,
+        })
+    )
 
     const navToggle = () => {
         setNavDescp(!navDescp)
@@ -23,12 +39,38 @@ function Navbar() {
 
     return (
         <>
-            {/* <div className='bg-black h-16 flex justify-start items-center w-full z-50'>
-                <span className='text-white px-4 text-xl'>GameDevUtopia</span>
-            </div> */}
+            <div className='bg-black justify-between h-[4.3rem] px-2 flex fixed items-center w-full z-50'>
+                <div className='flex justify-center items-center'>
+
+                    <Image
+                        src={GduLogo}
+                        alt="gdulogo"
+                        width={35}
+                        height={35}
+                        className="mx-4 aspect-auto object-contain text-black"
+                    />
+                    <span className='text-white text-2xl'>GameDevUtopia</span>
+                </div>
+                <FaIcons.FaBell className='absolute right-12' size={25} onClick={bellButtonHandler} />
+                <ToastContainer
+                    position="top-right"
+                    autoClose={2000}
+                    hideProgressBar={false}
+                    newestOnTop={false}
+                    closeOnClick
+                    rtl={false}
+                    pauseOnFocusLoss
+                    draggable
+                    pauseOnHover
+                    theme="dark"
+                />
+            </div>
+
+
+
             {showSidebar && <nav
-                className={'sm:w-fit w-full max-sm:bottom-3 fixed sm:top-[20%] bg-black z-50 pt-4 pb-1 sm:py-4 rounded-2xl cursor-default shadow-sm shadow-white sm:mx-1'}>
-                <ul className='w-full max-h-fit flex sm:flex-col items-center sm:items-start justify-evenly   '>
+                className={'sm:w-fit sm:h-[30rem] items-center w-full max-sm:bottom-3 fixed sm:top-[20%] bg-black z-50 pt-4 pb-1 sm:py-4 rounded-2xl cursor-default shadow-sm shadow-white sm:mx-1'}>
+                <ul className='w-full max-h-fit flex sm:gap-y-3 sm:flex-col items-center sm:items-start justify-evenly   '>
                     {!navDescp ? (<li
                         onClick={navToggle}
                         className='sm:flex justify-start hidden gap-5 items-center pl-6 pr-6 pt-0 pb-1 text-xl my-1.5'>
@@ -38,7 +80,7 @@ function Navbar() {
                         :
                         (<li
                             onClick={navToggle}
-                            className='flex justify-start gap-5 items-center pl-6 pr-6 pt-0 pb-1 text-xl my-1.5 text-white'>
+                            className='flex justify-start gap-5 items-center sm:px-6 pt-0 pb-1 text-xl my-0.5 text-white'>
                             <RxCross2 onClick={navToggle} fontWeight={900} /> <span>Close</span>
                         </li>)}
                     {SidebarData.map((item, index) => {
@@ -46,12 +88,13 @@ function Navbar() {
                             <div key={index} className='flex flex-col-reverse sm:flex-row sm:justify-start sm:gap-4 items-center'>
                                 <li
 
-                                    className={pathname == item.path ? "rounded-lg bg-[#3d64ff]  " + navClasses : navClasses}
+                                    className={pathname == item.path ? "rounded-lg bg-[#f3bc12]  " + navClasses : navClasses}
                                 >
                                 </li>
                                 <Link href={item.path}>
-                                    <div className={pathname == item.path ? 'flex justify-center sm:gap-7 text-[#3d64ff] items-center text-xl' : 'flex justify-center gap-7 items-center text-xl text-white'}>{item.icon}
-                                        {navDescp ? <div className='text-lg pr-4 w-32'>{item.title}</div> : null}
+                                    <div className={pathname == item.path ? 'flex justify-center sm:gap-7 text-[#f3bc12] items-center text-2xl' : 'flex justify-center sm:gap-7 items-center text-2xl text-white'}>
+                                        {item.icon}
+                                        {navDescp ? <div className='text-lg pr-4 w-40'>{item.title}</div> : null}
                                     </div>
                                 </Link>
                             </div>
