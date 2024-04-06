a472297c594860a7c0f22e3cf3e769c9d92426a1
MayuriKolhe-2003 <kolhem10@gmail.com>
2024-04-06 23:04:27 +0530

    deploy changes

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://127.0.0.1:8080/',
+  baseURL: 'https://gdu-club-website-be.onrender.com',
   timeout: 5000,
   headers: {
     'Content-Type': 'application/json',
