9093aac957f6325b5c7e2afd6aa795753e20032d
Kiran C. Kankariya <kirankankariya097+git@gmail.com>
2024-04-19 20:39:28 +0530

    loading time reducede by 50% for homepagegames section

--- src/app/page.js
+++ src/app/page.js
@@ -9,21 +9,12 @@ import Link from 'next/link'
 import EmblaCarousel from "@/components/Carousel/EmblaCarousel";
 import '../styles/embla.css'
 import axios from '../api/axios'
-const OPTIONS = { loop: true }
-
-let SLIDES = []
-async function fetchGames() {
-  await axios.get('/api/game/homepagegames')
-    .then((response) => {
-      const { games } = response.data;
-      SLIDES = games
-    })
-    .catch((error) => {
-      console.log(error)
-    })
+const OPTIONS = {
+  loop: true,
 }
+import { useEffect, useState } from "react";
 
-fetchGames()
+
 
 // const images = [
 //   "https://plus.unsplash.com/premium_photo-1675257062614-c1da6b8d83ee?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8Z2FtaW5nfGVufDB8fDB8fHww",
@@ -35,6 +26,21 @@ fetchGames()
 // ]
 
 export default function Home() {
+  const [SLIDES, setSlides] = useState([])
+  async function fetchGames() {
+    await axios.get('/api/game/homepagegames')
+      .then((response) => {
+        const { games } = response.data;
+        setSlides(games)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }
+
+  useEffect(() => {
+    fetchGames()
+  }, [])
   return (
     <>
       <LandingPage />
@@ -42,6 +48,8 @@ export default function Home() {
       <section id="games relative space-y-4 z-10">
         <h1 className="w-fit mx-auto txt-grad font-bold text-center text-3xl my-10">Games</h1>
         <EmblaCarousel slides={SLIDES} options={OPTIONS} />
+
+
         <span className="grid place-items-center">
           <Link href="/games" className="font-semibold text-lg p-2 ">
             <button className="bg-grad py-1 rounded-full">
--- src/components/Carousel/EmblaCarousel.jsx
+++ src/components/Carousel/EmblaCarousel.jsx
@@ -31,91 +31,18 @@ const EmblaCarousel = (props) => {
     onNextButtonClick
   } = usePrevNextButtons(emblaApi)
 
-  const setTweenNodes = useCallback((emblaApi) => {
-    tweenNodes.current = emblaApi.slideNodes().map((slideNode) => {
-      return slideNode.querySelector('.embla__slide__number')
-    })
-  }, [])
-
-  const setTweenFactor = useCallback((emblaApi) => {
-    tweenFactor.current = TWEEN_FACTOR_BASE * emblaApi.scrollSnapList().length
-  }, [])
-
-  const tweenScale = useCallback((emblaApi, eventName) => {
-    const engine = emblaApi.internalEngine()
-    const scrollProgress = emblaApi.scrollProgress()
-    const slidesInView = emblaApi.slidesInView()
-    const isScrollEvent = eventName === 'scroll'
-
-    emblaApi.scrollSnapList().forEach((scrollSnap, snapIndex) => {
-      let diffToTarget = scrollSnap - scrollProgress
-      const slidesInSnap = engine.slideRegistry[snapIndex]
-
-      slidesInSnap.forEach((slideIndex) => {
-        if (isScrollEvent && !slidesInView.includes(slideIndex)) return
-
-        if (engine.options.loop) {
-          engine.slideLooper.loopPoints.forEach((loopItem) => {
-            const target = loopItem.target()
-
-            if (slideIndex === loopItem.index && target !== 0) {
-              const sign = Math.sign(target)
-
-              if (sign === -1) {
-                diffToTarget = scrollSnap - (1 + scrollProgress)
-              }
-              if (sign === 1) {
-                diffToTarget = scrollSnap + (1 - scrollProgress)
-              }
-            }
-          })
-        }
-
-        const tweenValue = 1 - Math.abs(diffToTarget * tweenFactor.current)
-        const scale = numberWithinRange(tweenValue, 0, 1).toString()
-        const tweenNode = tweenNodes.current[slideIndex]
-        tweenNode.style.transform = `scale(${scale})`
-      })
-    })
-  }, [])
-
-  useEffect(() => {
-    if (!emblaApi) return
-
-    setTweenNodes(emblaApi)
-    setTweenFactor(emblaApi)
-    tweenScale(emblaApi)
-
-    emblaApi
-      .on('reInit', setTweenNodes)
-      .on('reInit', setTweenFactor)
-      .on('reInit', tweenScale)
-      .on('scroll', tweenScale)
-  }, [emblaApi, tweenScale, setTweenFactor, setTweenNodes])
 
   return (
     <div className="embla">
       <div className="embla__viewport" ref={emblaRef}>
-        <div className={`embla__container ${slides.length > 0 ? "h-[350px]" : ""}`}>
+        <div className={`embla__container ${slides.length > 0 ? "h-[325px]" : ""}`}>
           {slides.map((item, i) => (
             <>
-              {/* <div className="embla__slide" key={index}>
-              <div className="embla__slide__number relative">
-                <Image src={item.thumbnail} fill={true} alt="carousel-img" />
-                <span className='absolute z-10 bottom-0 px-2 bg-slate-900/30 w-full '>
-                <Collaborators developersArray={item.developer_ids}/>
-                <div className='absolute w-full bottom-0 border-transparent left-0 px-5 w-full overflow-hidden my-4'>
-                  <Collaborators developersArray={item.developer_ids} />
-                </div>
-                <h1 className='text-xl'>{item.title}</h1>
-                </span>
-              </div>
-            </div> */}
               <div key={i} className='embla__slide flex justify-center items-center z-20 font-bold '>
                 {/* <div className='max-w-[16rem] max-h-16 font-bold border-white animate-glow border-2 px-3 rounded-xl py-1 pb-3 text-overflow-ellipsis whitespace-no-wrap overflow-hidden text-center'>{item.title}
                 </div> */}
                 <div
-                  className="embla__slide__number relative flex-col group rounded-2xl  overflow-hidden bg-slate-800 duration-500 w-64 h-[20.5rem] shadow shadow-gray-600 mx-5 text-gray-50 p-2.5 grid justify-items-center bottom-3 z-10 "
+                  className="embla__slide__number relative flex-col group rounded-2xl  overflow-hidden bg-slate-800 duration-500 w-60 h-[18rem] shadow shadow-gray-600 mx-5 text-gray-50 p-2.5 grid justify-items-center bottom-3 z-10 "
                 >
                   <div className="text-center absolute"></div>
                   <div className="">
@@ -136,8 +63,8 @@ const EmblaCarousel = (props) => {
               </div> */}
 
                       <Collaborators developersArray={item.developer_ids} />
-                      <p className="w-56 font-normal line-clamp-2">
-                        {item.description}
+                      <p className="w-56 font-bold text-lg">
+                        {item.title}
                       </p>
                     </div>
                   </div>
