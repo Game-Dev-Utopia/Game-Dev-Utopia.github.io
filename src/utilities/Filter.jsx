e12f14e182fdda49b5f1210924cc026206633930
Kiran C. Kankariya <kirankankariya097+git@gmail.com>
2024-05-06 14:23:54 +0530

    filters design changes and gdu favicon added

--- src/utilities/Filter.jsx
+++ src/utilities/Filter.jsx
@@ -4,6 +4,7 @@ import {
   DropdownMenuCheckboxItem,
   DropdownMenuSeparator,
   DropdownMenuTrigger,
+
 } from "@/components/ui/dropdown-menu"
 import React from 'react'
 import { TiArrowSortedDown } from "react-icons/ti";
@@ -76,102 +77,113 @@ const Filter = ({ addToFilterList, setItem, removeFromFilterList, filterCount, c
     }
   ]
 
-
+  const clrFiltersFn = () => {
+    clearFilters();
+    const keys = [
+      "Puzzle",
+      "Action",
+      "Survival",
+      "Strategy",
+      "Adventure",
+      "Platformer",
+      "Shooting",
+      "Stealth",
+      "Incremental",
+      "Simulation",
+      "RPG",
+      "Casual",
+      'Mobile',
+      'PC',
+      'Quick',
+      'Heavy',
+      '2D',
+      '3D',
+      'Downloadable'
+    ];
+    setSelect(keys.reduce((obj, key) => ({ ...obj, [key]: false }), {}));
+  }
 
   const [selectDownloadable, setSelectDownloadable] = React.useState(false)
   return (
-    <div className='flex flex-wrap pb-2 sm:pb-1 justify-evenly md:justify-center bg-slate-900'>
-      {
-        menu.map((val, i) => (
-          <div
-            key={i}
-            className={`text-white my-2 w-[48%] min-[375px]:w-[32%] sm:w-auto px-1 sm:mx-1 cursor-pointer shadow-sm bg-grad-hover justify-center items-center bg-slate-800 shadow-gray-600 rounded-full  ${'hover:shadow-lg'}`}>
-            <DropdownMenu>
-              <DropdownMenuTrigger className="flex h-8 justify-center max-[400px]:text-xs mx-auto items-center outline-none font-medium text-sm ">
-                {val.title}
-                <TiArrowSortedDown className="flex items-center mt-1 ml-[0.2rem]" />
-              </DropdownMenuTrigger>
+    <div className="overflow-x-scroll">
+      <div className='flex overflow-x-scroll gap-1 pt-1 pb-2 sm:pb-1 md:justify-center md:items-center bg-slate-900 mx-2'>
+        <div
+          className={`${filterCount > 0 ? "visible sm:hidden" : "hidden"} sm:mx-1.5 h-7 mx-0.5 min-w-fit justify-center text-center max-[400px]:text-xs my-2 px-3 sm:px-4 flex items-center font-medium text-sm gap-2 cursor-pointer shadow-sm bg-grad-hover bg-slate-800  shadow-gray-600 rounded-full`}
+          onClick={clrFiltersFn}>
+          <RxCross2 size={15} />
+          Clear Filters
+        </div>
+        {
+          menu.map((val, i) => (
+            <div
+              key={i}
+              className={`text-white my-2 min-w-fit sm:w-auto px-1 sm:mx-1 cursor-pointer shadow-sm bg-grad-hover sm:justify-center items-center bg-slate-800 shadow-gray-600 rounded-full  ${'hover:shadow-lg'}`}>
+              <DropdownMenu>
+                <DropdownMenuTrigger
+                  className="h-7 flex items-center justify-center outline-none font-medium text-sm "
+                  onTouchMove={(e) => e.preventDefault()}>
+                  {val.title}
+                  <TiArrowSortedDown className="ml-0.5 relative top-[5%] sm:top-[12%]" />
+                </DropdownMenuTrigger>
 
-              <DropdownMenuContent>
-                {val.values.map((value, item) => (
-                  <DropdownMenuCheckboxItem
-                    onSelect={(event) => event.preventDefault()}
-                    key={value.id}
-                    checked={select[value.name]}
-                    onCheckedChange={() => {
-                      // setSelect2D(!select[i])
-                      setSelect((prev) => ({ ...prev, [value.name]: !prev[value.name] }))
-                      if (!select[value.name]) {
-                        addToFilterList(value.name)
-                      } else {
-                        removeFromFilterList(value.name)
-                      }
-                    }}
-                  >
-                    {value.name}
-                  </DropdownMenuCheckboxItem>
-                ))}
-              </DropdownMenuContent>
-            </DropdownMenu>
+                <DropdownMenuContent
+                  onScroll={(event) => event.preventDefault()}
+                >
+                  {val.values.map((value, item) => (
+                    <DropdownMenuCheckboxItem
+                      onSelect={(event) => event.preventDefault()}
+                      key={value.id}
+                      checked={select[value.name]}
+                      onCheckedChange={() => {
+                        // setSelect2D(!select[i])
+                        setSelect((prev) => ({ ...prev, [value.name]: !prev[value.name] }))
+                        if (!select[value.name]) {
+                          addToFilterList(value.name)
+                        } else {
+                          removeFromFilterList(value.name)
+                        }
+                      }}
+                    >
+                      {value.name}
+                    </DropdownMenuCheckboxItem>
+                  ))}
+                </DropdownMenuContent>
+              </DropdownMenu>
 
-          </div>
-        ))
-      }
+            </div>
 
-
-      <div className={`sm:ml-1 mx-0.5 sm:bg-grad-hover my-2 h-8 text-center cursor-pointer justify-center w-[48%] min-[375px]:w-[30%] md:w-auto max-[400px]:px-5 sm:px-5 max-[400px]:text-xs rounded-full items-center inline-flex text-sm font-medium shadow-sm shadow-gray-600 ${select.Downloadable ?
-        " bg-grad text-white"
-        :
-        "bg-slate-800 hover:bg-slate-600 sm:hover:bg-gradient-to-r sm:from-cyan-500 sm:to-blue-500"}`}
-        onClick={() => {
-          setSelect((prev) => ({ ...prev, ["Downloadable"]: !prev["Downloadable"] }))
-          if (!select["Downloadable"]) {
-            addToFilterList("Downloadable")
-          } else {
-            removeFromFilterList("Downloadable")
-          }
-        }}
-      >
-        Downloadable
-      </div>
+          ))
+        }
 
 
-      <div
-        className={`${filterCount > 0 ? "visible" : "invisible"} sm:mx-1.5 h-8 mx-0.5 w-[48%] md:w-auto min-[375px]:w-[30%] justify-center text-center max-[400px]:text-xs my-2 p-1 sm:px-4 flex items-center font-medium text-sm gap-2 cursor-pointer shadow-sm bg-grad-hover bg-slate-800  shadow-gray-600 rounded-full`}
-        onClick={() => {
-          clearFilters();
-          const keys = [
-            "Puzzle",
-            "Action",
-            "Survival",
-            "Strategy",
-            "Adventure",
-            "Platformer",
-            "Shooting",
-            "Stealth",
-            "Incremental",
-            "Simulation",
-            "RPG",
-            "Casual",
-            'Mobile',
-            'PC',
-            'Quick',
-            'Heavy',
-            '2D',
-            '3D',
-            'Downloadable'
-          ];
-          setSelect(keys.reduce((obj, key) => ({ ...obj, [key]: false }), {}));
-        }}>
-        <RxCross2 size={20} />
-        Clear Filters
-      </div>
+        <div className={`sm:ml-1 mx-0.5 sm:bg-grad-hover my-2 h-7 text-center cursor-pointer justify-center min-w-fit px-4 max-[400px]:text-xs rounded-full items-center inline-flex text-sm font-medium shadow-sm shadow-gray-600 ${select.Downloadable ?
+          " bg-grad text-white"
+          :
+          "bg-slate-800 hover:bg-slate-600 sm:hover:bg-gradient-to-r sm:from-cyan-500 sm:to-blue-500"}`}
+          onClick={() => {
+            setSelect((prev) => ({ ...prev, ["Downloadable"]: !prev["Downloadable"] }))
+            if (!select["Downloadable"]) {
+              addToFilterList("Downloadable")
+            } else {
+              removeFromFilterList("Downloadable")
+            }
+          }}
+        >
+          Downloadable
+        </div>
 
+        <div
+          className={`${filterCount > 0 ? "hidden sm:flex sm:visible items-center" : "invisible"} sm:mx-1.5 h-7 mx-0.5 min-w-fit justify-center text-center max-[400px]:text-xs my-2 px-3 sm:px-4 flex items-center font-medium text-sm gap-2 cursor-pointer shadow-sm bg-grad-hover bg-slate-800  shadow-gray-600 rounded-full`}
+          onClick={clrFiltersFn}>
+          <RxCross2 size={18} />
+          Clear Filters
+        </div>
 
-      {/* TODO: future scope all filters displayed under same hood(dropdown) */}
-      {/* <Toggle className="my-2 mx-5">      //not  a toggle its a dropdown
+        {/* TODO: future scope all filters displayed under same hood(dropdown) */}
+        {/* <Toggle className="my-2 mx-5">      //not  a toggle its a dropdown
           All
         </Toggle> */}
+      </div>
     </div>
   )
 }
