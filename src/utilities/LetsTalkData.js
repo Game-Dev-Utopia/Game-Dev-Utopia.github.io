b7db605646c000df166993377d86cdc00249b7b7
MayuriKolhe-2003 <kolhem10@gmail.com>
2024-06-03 23:14:53 +0530

    letstalk

--- src/utilities/LetsTalkData.js
+++ src/utilities/LetsTalkData.js
@@ -5,7 +5,7 @@ export const Lets_talkData = [
     title: "Business Proposal",
     text: "If you go over your organisation's or user limit, a member of the team will reach out about bespoke pricing. In the meantime, our collaborative features won't appear in accounts or users that are over the 100-account or 1,000-user limit.",
     active: false,
-    img: "https://cdni.iconscout.com/illustration/premium/thumb/woman-login-into-banking-app-5194430-4341049.png",
+    img: "/images/1.png",
     borderColor: "#44ff00",
     stepsData: [
       {
@@ -120,7 +120,7 @@ export const Lets_talkData = [
     title: "Project Proposal",
     text: "If you go over your organisations or user limit, a member of the team will reach out about bespoke pricing. In the meantime, our collaborative features won't appear in accounts or users that are over the 100-account or 1,000-user limit.",
     active: false,
-    img: "https://cdni.iconscout.com/illustration/premium/thumb/login-page-4468581-3783954.png?f=webp",
+    img: "/images/2.png",
     borderColor: "#ebf707",
     stepsData: [
       {
@@ -214,7 +214,7 @@ export const Lets_talkData = [
     title: "Join Us",
     text: "If you go over your organisations or user limit, a member of the team will reach out about bespoke pricing. In the meantime, our collaborative features won't appear in accounts or users that are over the 100-account or 1,000-user limit.",
     active: false,
-    img: "https://fiftytwodigital.com/wp-content/uploads/2022/02/joinus.png",
+    img: "/images/3.png",
     borderColor: "#fa2335",
     stepsData: [
       {
@@ -300,7 +300,7 @@ export const Lets_talkData = [
     title: "Query",
     text: "If you go over your organisations or user limit, a member of the team will reach out about bespoke pricing. In the meantime, our collaborative features won't appear in accounts or users that are over the 100-account or 1,000-user limit.",
     active: false,
-    img: "https://cdn-icons-png.flaticon.com/512/9821/9821130.png",
+    img: "/images/4.png",
     borderColor: "#f59842",
     stepsData: [
       {
@@ -368,7 +368,7 @@ export const Lets_talkData = [
     title: "Event Hosting",
     text: "If you go over your organisations or user limit, a member of the team will reach out about bespoke pricing. In the meantime, our collaborative features won't appear in accounts or users that are over the 100-account or 1,000-user limit.",
     active: false,
-    img: "https://png.pngtree.com/png-vector/20220811/ourmid/pngtree-radio-show-host-and-guest-flat-color-vector-faceless-characters-png-image_5844342.png",
+    img: "/images/5.png",
     borderColor: "#b2f084",
     stepsData: [
       {
