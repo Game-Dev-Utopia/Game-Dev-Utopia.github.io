3db5a0cba13750d457e489954c34417e31895749
Mayuri Kolhe <77161474+MayuriKolhe-2003@users.noreply.github.com>
2024-08-18 22:57:43 +0530

    generatestatisparam

--- src/app/games/[id]/page.js
+++ src/app/games/[id]/page.js
@@ -5,6 +5,9 @@ import { getRequest } from '@/api/api';
 
 export async function generateStaticParams() {
   try {
+    const response = await getRequest('/api/game/getallgames');
+    const games = await response.games;
+    
     return games.map(e => ({
       id: e._id.toString(),
     }));
