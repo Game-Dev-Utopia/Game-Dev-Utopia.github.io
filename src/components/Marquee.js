7bb71bd146505db7382f12423888ce387dde1982
Mayuri Kolhe <77161474+MayuriKolhe-2003@users.noreply.github.com>
2024-09-08 17:49:41 +0530

    form response, marquee changes

--- src/components/Marquee.js
+++ src/components/Marquee.js
@@ -3,12 +3,9 @@ import React from 'react';
 function Marquee() {
     return (
         <div className="w-full overflow-hidden whitespace-nowrap bg-[#121213] text-white py-1 fixed top-14 z-40">
-            <p className="inline-block animate-marquee pl-full font-semibold">
-             ️ 💻  The website is under development... Stay Tuned ! ️💻 
-             ️ The website is under development... Stay Tuned ! ️💻 
-             The website is under development... Stay Tuned ! ️💻 
-             The website is under development... Stay Tuned ! ️💻 
-            </p>
+            <pre className="inline-block animate-marquee pl-full font-semibold">
+             ️💻  The website is under development... Stay Tuned ! ️💻      The website is under development... Stay Tuned ! ️💻        The website is under development... Stay Tuned ! ️💻         The website is under development... Stay Tuned ! ️💻
+            </pre>
         </div>
     );
 }
