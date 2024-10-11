4ee62ca02308d2da3685ffd0cf0d371a01e60aa7
Mayuri Kolhe <77161474+MayuriKolhe-2003@users.noreply.github.com>
2024-10-11 19:44:50 +0530

    gtag configure

--- src/app/layout.js
+++ src/app/layout.js
@@ -22,7 +22,7 @@ export default function RootLayout({ children }) {
           new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
           j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-G-GGQ8NSHNDR');`} // Place your GTM code here
+          })(window,document,'script','dataLayer','GTM-G-GGQ8NSHNDR');`} 
         </Script>
       </head>
       <body className={"bg-slate-900"}>
