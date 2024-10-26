df2a3356d7f51e8672966b9a721bec4df25ee4b5
harshgaggar19 <harshgaggar19@gmail.com>
2024-10-26 20:36:17 +0530

    issue G-24 and G-27

--- src/components/ArtGallery/page.js
+++ src/components/ArtGallery/page.js
@@ -1,196 +1,212 @@
-"use client"
+"use client";
 import React, { useEffect, useRef, useState } from "react";
-import Link from 'next/link'; 
-import { FaFire } from 'react-icons/fa';
-import ShareIcon from '@mui/icons-material/Share';
+import Link from "next/link";
+import { FaFire } from "react-icons/fa";
+import ShareIcon from "@mui/icons-material/Share";
 import Avatar from "@mui/material/Avatar";
-import { getRequest } from '@/api/api';
+import { getRequest } from "@/api/api";
 import "./index.css";
 import Collaborators from "../GamePage/Collaborators";
 import Image from "next/image";
 
 const ArtGallery = () => {
-  const [galleryData, setGalleryData] = useState([]);
-  const dataFetchedRef = useRef(false);
-  const [loading, setLoading] = useState(true);
+	const [galleryData, setGalleryData] = useState([]);
+	const dataFetchedRef = useRef(false);
+	const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    console.log("Component Mounted");
-    if (!dataFetchedRef.current) {
-      const fetchData = async () => {
-        try {
-          const response = await getRequest('api/design/getdesigns');
-          setGalleryData(response);
-          dataFetchedRef.current = true;
-        } catch (error) {
-          console.error('Error fetching design data:', error);
-        } finally {
-          setLoading(false);
-        }
-      };
-      fetchData();
-    } else {
-      setLoading(false);
-    }
-  }, []);
+	useEffect(() => {
+		console.log("Component Mounted");
+		if (!dataFetchedRef.current) {
+			const fetchData = async () => {
+				try {
+					const response = await getRequest("api/design/getdesigns");
+					setGalleryData(response);
+					dataFetchedRef.current = true;
+				} catch (error) {
+					console.error("Error fetching design data:", error);
+				} finally {
+					setLoading(false);
+				}
+			};
+			fetchData();
+		} else {
+			setLoading(false);
+		}
+	}, []);
 
-  useEffect(() => {
-    console.log("Component Rendered");
-  });
+	useEffect(() => {
+		console.log("Component Rendered");
+	});
 
-  
-  const [likes, setLikes] = useState({});
-  const [liked, setLiked] = useState({});
-  const [shared, setShared] = useState({});
-  const [shares, setShares] = useState({});
-  const [showFullDescription, setShowFullDescription] = useState({});
+	const [likes, setLikes] = useState({});
+	const [liked, setLiked] = useState({});
+	const [shared, setShared] = useState({});
+	const [shares, setShares] = useState({});
+	const [showFullDescription, setShowFullDescription] = useState({});
 
-  useEffect(() => {
-    const savedLikes = JSON.parse(localStorage.getItem("likes")) || {};
-    const savedShares = JSON.parse(localStorage.getItem("shares")) || {};
-    setLikes(savedLikes);
-    setShares(savedShares);
-  }, []);
+	useEffect(() => {
+		const savedLikes = JSON.parse(localStorage.getItem("likes")) || {};
+		const savedShares = JSON.parse(localStorage.getItem("shares")) || {};
+		setLikes(savedLikes);
+		setShares(savedShares);
+	}, []);
 
-  const handleLike = (index) => {
-    if (!liked[index]) {
-      setLiked((prevLiked) => ({ ...prevLiked, [index]: true }));
-      setLikes((prevLikes) => {
-        const newLikes = { ...prevLikes, [index]: (prevLikes[index] || 0) + 1 };
-        localStorage.setItem("likes", JSON.stringify(newLikes));
-        return newLikes;
-      });
-    }
-  };
+	const handleLike = (index) => {
+		if (!liked[index]) {
+			setLiked((prevLiked) => ({ ...prevLiked, [index]: true }));
+			setLikes((prevLikes) => {
+				const newLikes = { ...prevLikes, [index]: (prevLikes[index] || 0) + 1 };
+				localStorage.setItem("likes", JSON.stringify(newLikes));
+				return newLikes;
+			});
+		}
+	};
 
-  const handleShare = (index) => {
-    if (!shared[index]) {
-      setShared((prevShared) => ({ ...prevShared, [index]: true }));
-      setShares((prevShares) => {
-        const newShares = { ...prevShares, [index]: (prevShares[index] || 0) + 1 };
-        localStorage.setItem("shares", JSON.stringify(newShares));
-        return newShares;
-      });
-    }
-  };
+	const handleShare = (index) => {
+		if (!shared[index]) {
+			setShared((prevShared) => ({ ...prevShared, [index]: true }));
+			setShares((prevShares) => {
+				const newShares = {
+					...prevShares,
+					[index]: (prevShares[index] || 0) + 1,
+				};
+				localStorage.setItem("shares", JSON.stringify(newShares));
+				return newShares;
+			});
+		}
+	};
 
-  const handleVideoEnded = (event, index) => {
-    const videoElement = event.target;
-    videoElement.currentTime = 0;
-    videoElement.play();
-  };
+	const handleVideoEnded = (event, index) => {
+		const videoElement = event.target;
+		videoElement.currentTime = 0;
+		videoElement.play();
+	};
 
-  const toggleShowFullDescription = (index) => {
-    setShowFullDescription((prevShowFullDescription) => ({
-      ...prevShowFullDescription,
-      [index]: !prevShowFullDescription[index],
-    }));
-  };
+	const toggleShowFullDescription = (index) => {
+		setShowFullDescription((prevShowFullDescription) => ({
+			...prevShowFullDescription,
+			[index]: !prevShowFullDescription[index],
+		}));
+	};
 
-const [hoveredIndex, setHoveredIndex] = useState(null);
-  const [fadeTimeout, setFadeTimeout] = useState(null);
+	const [hoveredIndex, setHoveredIndex] = useState(null);
+	const [fadeTimeout, setFadeTimeout] = useState(null);
 
-  const handleDescriptionHover = (index) => {
-    setHoveredIndex(index);
-  };
+	const handleDescriptionHover = (index) => {
+		setHoveredIndex(index);
+	};
 
-  const getFileType = (url) => {
-    const fileExtension = url.split('.').pop().toLowerCase();
-    if (['jpg', 'jpeg', 'png', 'gif', 'bmp'].includes(fileExtension)) {
-      return 'image';
-    } else if (['mp4', 'webm', 'ogg', 'mkv', 'mov'].includes(fileExtension)) {
-      return 'video';
-    }
-    return 'unknown';
-  };
+	const getFileType = (url) => {
+		const fileExtension = url.split(".").pop().toLowerCase();
+		if (["jpg", "jpeg", "png", "gif", "bmp"].includes(fileExtension)) {
+			return "image";
+		} else if (["mp4", "webm", "ogg", "mkv", "mov"].includes(fileExtension)) {
+			return "video";
+		}
+		return "unknown";
+	};
 
-  useEffect(() => {
-    if (hoveredIndex !== null) {
-      const timeout = setTimeout(() => {
-        setHoveredIndex(null);
-      }, 2000);
+	useEffect(() => {
+		if (hoveredIndex !== null) {
+			const timeout = setTimeout(() => {
+				setHoveredIndex(null);
+			}, 2000);
 
-      const descriptionElement = document.getElementById(`description-${hoveredIndex}`);
-      if (descriptionElement) {
-        descriptionElement.classList.add('fade-out');
-      }
+			const descriptionElement = document.getElementById(
+				`description-${hoveredIndex}`
+			);
+			if (descriptionElement) {
+				descriptionElement.classList.add("fade-out");
+			}
 
-      setFadeTimeout(timeout);
-    }
+			setFadeTimeout(timeout);
+		}
 
-    return () => {
-      clearTimeout(fadeTimeout);
+		return () => {
+			clearTimeout(fadeTimeout);
 
-      const descriptionElement = document.getElementById(`description-${hoveredIndex}`);
-      if (descriptionElement) {
-        descriptionElement.classList.remove('fade-out');
-      }
-    };
-  }, [hoveredIndex, fadeTimeout]);
+			const descriptionElement = document.getElementById(
+				`description-${hoveredIndex}`
+			);
+			if (descriptionElement) {
+				descriptionElement.classList.remove("fade-out");
+			}
+		};
+	}, [hoveredIndex, fadeTimeout]);
 
+	return (
+		<div className="design-page">
+			<h1 className="txt-grad font-bold text-center text-3xl">Designs</h1>
+			<hr />
 
-  return (
-    <div className="design-page">
-      <h1 className='txt-grad font-bold text-center text-3xl'>Designs</h1>
-      <hr />
-
-      <div className="image-gallery">
-        {galleryData.map((item, index) => (
-          <div key={index} className={`item img${index + 1}`}>
-            <Link href={`/designs/${item._id}`} passHref>
-                  {getFileType(item.designs[0]) === 'video' ? (
-                    <video loop
-              onMouseEnter={(e) => e.target.play()}
-              onMouseLeave={(e) => e.target.pause()} muted onEnded={(e) => handleVideoEnded(e, index)} className={`video-item video${index + 1}`}
-                    style={{ borderRadius: '15px' }}>
-                      <source src={item.designs[0]} type="video/mp4" />
-                      Your browser does not support the video tag.
-                    </video>
-                  ) : (
-                    <Image
-                    fill={true}
-                      src={item.designs[0]}
-                      alt={`Image ${index}`}
-                      className={`item img${index + 1}`}
-                      quality={75}
-                    />
-                  )}
-                </Link>
-            <div className="overlay" >
-              <div className="content">
-                <div className="title">{item.title}</div>
-                <div className="developer-info">
-                  <span><Collaborators developersArray={item.developer_ids} /></span>
-                </div>
-              <div className="description">
-                  {showFullDescription[index]
-                    ? item.description
-                    : `${item.description.slice(0, 70)}...`}
-                  <span
-                    className="read-more"
-                    onClick={() => toggleShowFullDescription(index)}
-                  >
-                    {showFullDescription[index] ? "   Read less" : "Read more"}
-                  </span>
-                </div>
-              </div>
-              <div className="icons">
-                <div className="like-icon" onClick={() => handleLike(index)}>
-                  <FaFire style={{ background: "transparent" }} />
-                  <span className="like-count">{likes[index] || 0}</span>
-                </div>
-                <div className="share-icon" onClick={() => handleShare(index)}>
-                  <ShareIcon style={{ background: "transparent" }} />
-                  <span className="share-count">{shares[index] || 0}</span>
-                </div>
-              </div>
-            </div>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
+			<div className="image-gallery">
+				{galleryData.map((item, index) => (
+					<div key={index} className={`item img${index + 1}`}>
+						<Link href={`/designs/${item._id}`} passHref>
+							{getFileType(item.designs[0]) === "video" ? (
+								<video
+									loop
+									onMouseEnter={(e) => e.target.play()}
+									onMouseLeave={(e) => e.target.pause()}
+									muted
+									onEnded={(e) => handleVideoEnded(e, index)}
+									className={`video-item video${index + 1}`}
+									style={{ borderRadius: "15px" }}
+								>
+									<source src={item.designs[0]} type="video/mp4" />
+									Your browser does not support the video tag.
+								</video>
+							) : (
+								<Image
+									fill={true}
+									src={item.designs[0]}
+									alt={`Image ${index}`}
+									className={`item img${index + 1}`}
+									quality={75}
+								/>
+							)}
+							<div className="overlay">
+								<div className="content">
+									<div className="title">{item.title}</div>
+									<div className="developer-info">
+										<span>
+											<Collaborators developersArray={item.developer_ids} />
+										</span>
+									</div>
+									<div className="description">
+										{showFullDescription[index]
+											? item.description
+											: `${item.description.slice(0, 70)}...`}
+										<span
+											className="read-more"
+											onClick={() => toggleShowFullDescription(index)}
+										>
+											{showFullDescription[index]
+												? "   Read less"
+												: "Read more"}
+										</span>
+									</div>
+								</div>
+								<div className="icons">
+									<div className="like-icon" onClick={() => handleLike(index)}>
+										<FaFire style={{ background: "transparent" }} />
+										<span className="like-count">{likes[index] || 0}</span>
+									</div>
+									<div
+										className="share-icon"
+										onClick={() => handleShare(index)}
+									>
+										<ShareIcon style={{ background: "transparent" }} />
+										<span className="share-count">{shares[index] || 0}</span>
+									</div>
+								</div>
+							</div>
+						</Link>
+					</div>
+				))}
+			</div>
+		</div>
+	);
 };
 
 export default ArtGallery;
-                
--- src/components/DesignDetails.js
+++ src/components/DesignDetails.js
@@ -1,49 +1,61 @@
 /* eslint-disable @next/next/no-img-element */
 "use client";
-import Image from 'next/image';
-import React from 'react';
+import Image from "next/image";
+import React from "react";
 
-export default function GameDesignDetails({designData}) {
-  const [selectedAwardIndex, setSelectedAwardIndex] = React.useState(0);
-  const acheivementArray =designData.achievements;
-  return (
-    <section className="game-design-details mx-2 pt-4">
-      <div className="grid md:grid-cols-2 grid-cols-1 gap-3">
-        <div className="section-content p-6 rounded-2xl bg-[url('/images/games_section_bg_illustration.jpg')] bg-no-repeat bg-right hover:scale-[1.01] hover:shadow-md hover:shadow-[#fdb714] duration-200">
-          <h1 className="text-3xl text-headings font-semibold py-2">About the Design</h1>
-          <div className='glass_bar flex justify-between items-center py-2'>
-            <div className='glass1'></div>
-            <div className='glass3'></div>
-            <div className='glass2'></div>
-          </div>
-          <p className='py-6 text-white'>
-            {designData.description}
-          </p>
-        </div>
-        <div className="section-content p-6 h-full rounded-2xl bg-[url('/images/games_section_bg_illustration.jpg')] bg-no-repeat bg-right  hover:scale-[1.01] hover:shadow-md hover:shadow-[#fdb714] duration-200">
-          <h1 className="text-3xl text-headings font-semibold py-2">Achievements</h1>
-          <div className='glass_bar flex justify-between items-center py-2'>
-            <div className='glass1'></div>
-            <div className='glass3'></div>
-            <div className='glass2'></div>
-          </div>
-          <div className=" gap-2 my-6 ">
-            <div className='flex flex-row'>{"1234".split("").map((item, i) => (
-              <span
-                onClick={() => setSelectedAwardIndex(i)}
-                className={`relative p-3 rounded-md mx-2 duration-100 ${(selectedAwardIndex === i) ? 'border-2 border-[#fdb714] border-opacity-50' : 'border-2 border-transparent'}`} key={item}>
-                <Image fill={true} src="/trophy.svg" alt="trophy" />
-                <p className='text-lg absolute p-2 -top-2 -right-2'>
-                  🥇
-                </p>
-              </span>
-            ))}</div>
-          </div>
-          <p className='py-6 text-white' >
-            {acheivementArray[selectedAwardIndex]}
-          </p>
-        </div>
-      </div>
-    </section>
-  );
+export default function GameDesignDetails({ designData }) {
+	const [selectedAwardIndex, setSelectedAwardIndex] = React.useState(0);
+	const acheivementArray = designData.achievements;
+	return (
+		<section className="game-design-details mx-2 pt-4">
+			<div className="grid md:grid-cols-2 grid-cols-1 gap-3">
+				<div className="section-content p-6 rounded-2xl bg-[url('/images/games_section_bg_illustration.jpg')] bg-no-repeat bg-right hover:scale-[1.01] hover:shadow-md hover:shadow-[#fdb714] duration-200">
+					<h1 className="text-3xl text-headings font-semibold py-2">
+						About the Design
+					</h1>
+					<div className="glass_bar flex justify-between items-center py-2">
+						<div className="glass1"></div>
+						<div className="glass3"></div>
+						<div className="glass2"></div>
+					</div>
+					<p className="py-6 text-white">{designData.description}</p>
+				</div>
+				{acheivementArray.length > 0 ? (
+					<div className="section-content p-6 h-full rounded-2xl bg-[url('/images/games_section_bg_illustration.jpg')] bg-no-repeat bg-right  hover:scale-[1.01] hover:shadow-md hover:shadow-[#fdb714] duration-200">
+						<h1 className="text-3xl text-headings font-semibold py-2">
+							Achievements
+						</h1>
+						<div className="glass_bar flex justify-between items-center py-2">
+							<div className="glass1"></div>
+							<div className="glass3"></div>
+							<div className="glass2"></div>
+						</div>
+						<div className=" gap-2 my-6 ">
+							<div className="flex flex-row">
+								{"1234".split("").map((item, i) => (
+									<span
+										onClick={() => setSelectedAwardIndex(i)}
+										className={`relative p-3 rounded-md mx-2 duration-100 ${
+											selectedAwardIndex === i
+												? "border-2 border-[#fdb714] border-opacity-50"
+												: "border-2 border-transparent"
+										}`}
+										key={item}
+									>
+										<Image fill={true} src="/trophy.svg" alt="trophy" />
+										<p className="text-lg absolute p-2 -top-2 -right-2">🥇</p>
+									</span>
+								))}
+							</div>
+						</div>
+						<p className="py-6 text-white">
+							{acheivementArray[selectedAwardIndex]}
+						</p>
+					</div>
+				) : (
+					<></>
+				)}
+			</div>
+		</section>
+	);
 }
