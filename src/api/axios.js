0d447382c9bcc96f3679905c496c9e0d8caeab10
Mihir C. Ranade <60816002+mast-maula-mihir@users.noreply.github.com>
2024-11-10 16:52:08 +0530

    Update axios.js to prepend NEXT_PUBLIC_ to the BASE_URL env

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: process.env.BASE_URL || 'https://gdu-club-website-be.onrender.com',
+  baseURL: process.env.NEXT_PUBLIC_BASE_URL || 'https://gdu-club-website-be.onrender.com',
   timeout: 50000,
   headers: {
     'Content-Type': 'application/json',
