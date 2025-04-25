5fb25c000009a60776d2c378ee4cf1cbc59ce6e0
Mihir Ranade <techwizard01@hotmail.com>
2025-04-25 08:01:33 +0530

    Discord SSO :: changed the login url

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -134,7 +134,7 @@ function Navbar() {
                         </div>
                     ) : (
                         // Show Discord login button if not logged in
-                        <Link href={process.env.NEXT_PUBLIC_BASE_URL + "/login"}>
+                        <Link href={process.env.NEXT_PUBLIC_BASE_URL + "/api/auth/login"}>
                             <div className='flex items-center justify-center bg-sky-500 text-white px-4 py-2 rounded-lg hover:bg-sky-600 transition-all duration-300'>
                                 <FaIcons.FaDiscord size={20} className='mr-2' />
                                 <span className='text-sm sm:text-lg font-semibold'>Login</span>
